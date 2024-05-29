cae79738dce242d5369e207811572c56e9dcf1bf
Himanshu Tiwari <himanshu554466@gmail.com>
2024-05-29 17:39:25 +0530

    backend is connected with frontnd suceesfully

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
 const PORT = process.env.PORT || 4000;
 const URI = process.env.Mongo_URI;
 
